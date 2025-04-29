8ea162e29b484cda28a98bda6cb5db6d038fd189
Firdavs <76095334+FerNikoMF@users.noreply.github.com>
2025-04-29 21:39:07 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const ALLOWED_URL = process.env.ALLOWED_URL || 'https://google.com';
+const ALLOWED_URL = process.env.ALLOWED_URL || 'https://admin.gilam-market.uz';
 
 module.exports = {
   ALLOWED_URL
